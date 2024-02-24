bee608fa457c841102a9ae80a6a29b1bda64978e
Goo Kim <daekeun7@gmail.com>
2024-02-24 14:28:10 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,20 @@
 
 import streamlit as st
 import time
+# from langchain.llms import CTransformers
+from langchain_community.llms import CTransformers
 
-st.title('Goo Kim')
+llm = CTransformers(
+    model = "llama-2-7b-chat.ggmlv3.q2_K.bin",
+    model_type = "llama"
+)
+
+st.title('AI 시인 - powered by gKim')
+
 content = st.text_input('시의 주제를 제시해주세요')
 
 if st.button('시 작성 요청하기'):
     with st.spinner('Wait for it...'):
-        time.sleep(3)
-        result = content + "에 대한 시를 써줘"
+        # time.sleep(3)
+        result = llm.predict("write a poem about " + content + ": ")
         st.write(result)
